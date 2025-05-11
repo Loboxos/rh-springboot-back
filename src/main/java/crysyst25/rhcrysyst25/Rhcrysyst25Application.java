1e4a171457b445824d8881bbef674d6017914f03
loboxos <vcristian528@gmail.com>
2025-05-11 20:57:20 -0300

    primer commit estructura mini proyecto back end en spring de rh

---
+++ src/main/java/crysyst25/rhcrysyst25/Rhcrysyst25Application.java
@@ -0,0 +1,13 @@
+package crysyst25.rhcrysyst25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rhcrysyst25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rhcrysyst25Application.class, args);
+	}
+
+}
---
+++ src/test/java/crysyst25/rhcrysyst25/Rhcrysyst25ApplicationTests.java
@@ -0,0 +1,13 @@
+package crysyst25.rhcrysyst25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rhcrysyst25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
